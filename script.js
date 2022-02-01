20a02bef47339fff9f26a57f7ffa06f40358691a
tykavanaugh <tykavanaugh@gmail.com>
2022-02-01 18:50:24 -0500

    testing

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 
-const 
+const handleMouseOver = (event) => {
+  event.target.classList.add('color')
+}
 
 const main = () => {
   const container = document.getElementById('main-container')
-
-  for (let i = 0; i < 16; i++){
-    for (let j = 0; j < 16; j++){
+  for (let i = 0; i < 128; i++){
+    for (let j = 0; j < 128; j++){
       let square = document.createElement('div')
       square.textContent = " "
       square.classList.add('square')
       square.setAttribute('style',`grid-row-start: ${i} grid-row-end: ${i} grid-column-start: ${j} grid-column-end: ${j}`)
+      square.addEventListener("mouseover",(event => handleMouseOver(event)))
       container.appendChild(square)
     }
   }
